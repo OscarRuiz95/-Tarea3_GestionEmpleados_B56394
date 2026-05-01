881430e33786d011113e4fe38f49c3d4630c4c67
Oscar Ruiz <oscarruizperez95@gmail.com>
2026-05-01 12:12:19 -0600

    Creacion y configuracion de GestionEmpleados.BL

---
+++ GestionEmpleados.BL/Class1.cs
@@ -0,0 +1,70 @@
+﻿using GestionEmpleados.DA;
+using GestionEmpleados.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionEmpleados.BL
+{
+    public class EmpleadoRepository : IEmpleadoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public EmpleadoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Empleado> ObtenerTodos() =>
+            _context.Empleados.AsNoTracking().ToList();
+
+        public Empleado ObtenerPorId(int id) =>
+            _context.Empleados.Find(id);
+
+        public IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino)
+        {
+            if (string.IsNullOrWhiteSpace(termino))
+                return ObtenerTodos();
+
+            termino = termino.ToLower();
+            return _context.Empleados
+                .Where(e => e.Nombre.ToLower().Contains(termino) ||
+                            e.Apellidos.ToLower().Contains(termino) ||
+                            e.Departamento.ToLower().Contains(termino))
+                .AsNoTracking()
+                .ToList();
+        }
+
+        public IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string busqueda)
+        {
+            var query = BuscarPorNombreODepartamento(busqueda);
+            return query.Skip((pagina - 1) * tamano).Take(tamano);
+        }
+
+        public int ContarTotal(string busqueda) =>
+            BuscarPorNombreODepartamento(busqueda).Count();
+
+        public void Agregar(Empleado empleado)
+        {
+            _context.Empleados.Add(empleado);
+            _context.SaveChanges();
+        }
+
+        public void Actualizar(Empleado empleado)
+        {
+            _context.Empleados.Update(empleado);
+            _context.SaveChanges();
+        }
+
+        public void ToggleActivo(int id)
+        {
+            var empleado = _context.Empleados.Find(id);
+            if (empleado != null)
+            {
+                empleado.Activo = !empleado.Activo;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ GestionEmpleados.BL/Interface1.cs
@@ -0,0 +1,19 @@
+﻿using GestionEmpleados.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestionEmpleados.BL
+{
+    public interface IEmpleadoRepository
+    {
+        IEnumerable<Empleado> ObtenerTodos();
+        Empleado ObtenerPorId(int id);
+        IEnumerable<Empleado> BuscarPorNombreODepartamento(string termino);
+        IEnumerable<Empleado> ObtenerPaginado(int pagina, int tamano, string busqueda);
+        int ContarTotal(string busqueda);
+        void Agregar(Empleado empleado);
+        void Actualizar(Empleado empleado);
+        void ToggleActivo(int id);
+    }
+}
